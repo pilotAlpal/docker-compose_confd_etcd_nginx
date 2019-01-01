4a4a9d51a03f0b41a67153547b810e0f1441d9be
pilotAlpal <lsegar01@ucm.es>
2019-01-01 14:47:17 +0100

    quick-start example working with 3 containers

--- etcdCurl/insertkeys.sh
+++ etcdCurl/insertkeys.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -L http://etcd-server:2379/version
+#curl -L http://etcd-server:2379/version
+curl -L http://etcd-server:2379/v2/keys/myapp/database/url -XPUT -d value="db.example.com"
+curl -L http://etcd-server:2379/v2/keys/myapp/database/user -XPUT -d value="rob"
