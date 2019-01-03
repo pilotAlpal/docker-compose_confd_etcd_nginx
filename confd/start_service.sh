0bc055c3ec6fbdabc029638fe1351fb934063d22
Lucas Segarra <lucas.segarra1@ibm.com>
2019-01-03 17:45:38 +0100

    confd syncs partially myapp as in advanced example, nginx exits with ERROR

--- confd/start_service.sh
+++ confd/start_service.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+#rc-service nginx start
 /go/bin/confd -onetime -backend etcd -node http://etcd-server:2379 
-cat /tmp/myconfig.conf
+cat /tmp/myapp.conf
+nginx -g 'daemon off;'
 
--- etcdCurl/insertkeys.sh
+++ etcdCurl/insertkeys.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #curl -L http://etcd-server:2379/version
-curl -L http://etcd-server:2379/v2/keys/myapp/database/url -XPUT -d value="db.example.com"
-curl -L http://etcd-server:2379/v2/keys/myapp/database/user -XPUT -d value="rob"
+curl -L http://etcd-server:2379/v2/keys/myapp/subdomain -XPUT -d value="myapp"
+curl -L http://etcd-server:2379/v2/keys/myapp/upstream/app2 -XPUT -d value="10.0.1.100:80"
+curl -L http://etcd-server:2379/v2/keys/myapp/upstream/app1 -XPUT -d value="10.0.1.101:80"
