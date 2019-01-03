dd303f31a6bc5f9060364fd913e53273356cdea2
Lucas Segarra <lucas.segarra1@ibm.com>
2019-01-03 11:57:28 +0100

    python flask service added

---
+++ pyApp/dummy-server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import time
+import datetime
+
+app=Flask(__name__)
+@app.route('/time')
+def time():
+   ts = time.time()
+   dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+   return 'Date: %s' % dt
+
+@app.route('/greet')
+def greet():
+   return 'Hello buddy'
+
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0",port=8081)
+
