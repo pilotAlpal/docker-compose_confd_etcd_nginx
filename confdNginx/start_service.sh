e46ecd44fd1a5e07ce28fc3eb49fbdffe9d221c5
Lucas Segarra <lucas.segarra1@ibm.com>
2019-01-04 23:15:37 +0100

    specifying managed file when launching nginx

--- confdNginx/start_service.sh
+++ confdNginx/start_service.sh
@@ -4,5 +4,5 @@
 #nginx -g 'daemon off;' &
 /go/bin/confd -onetime -backend etcd -node http://etcd-server:2379 
 cat /tmp/myapp.conf
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' -c /tmp/myapp.conf
 
