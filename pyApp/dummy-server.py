c5b9189344aa5c867b7e774809d9e28d1cf7b4e2
Lucas Segarra <lucas.segarra1@ibm.com>
2019-01-04 23:34:23 +0100

    avoiding problems with function called time

--- pyApp/dummy-server.py
+++ pyApp/dummy-server.py
@@ -3,12 +3,11 @@ import time
 import datetime
 
 app=Flask(__name__)
-@app.route('/time')
-def time():
-    return 'Date'
-#   ts = time.time()
-#   dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-#   return 'Date: %s' % dt
+@app.route('/dtime')
+def dtime():
+   ts = time.time()
+   dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+   return 'Date: %s' % dt
 
 @app.route('/greet')
 def greet():
