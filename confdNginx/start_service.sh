101557b93c15e8da3c33754fc1c5d84b6b7d791f
Lucas Segarra <lucas.segarra1@ibm.com>
2019-01-04 12:58:14 +0100

    launching nginx before confd 4 avoiding service not found ERROR, removed openrc add, and renamed confd service as confd-nginx

--- confdNginx/start_service.sh
+++ confdNginx/start_service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #rc-service nginx start
+nginx
 /go/bin/confd -onetime -backend etcd -node http://etcd-server:2379 
 cat /tmp/myapp.conf
-nginx -g 'daemon off;'
+#nginx -g 'daemon off;'
 
