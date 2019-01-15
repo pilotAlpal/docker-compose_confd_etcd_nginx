de5719ca5aaade3aaa099190f9e8aa2d45df859b
pilotAlpal <lsegar01@ucm.es>
2019-01-15 19:05:18 +0100

    serving static content from html file with nginx and python app's response for /greet

--- confdNginx/start_service.sh
+++ confdNginx/start_service.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#rc-service nginx start
-#nginx 
-#nginx -g 'daemon off;' &
 /go/bin/confd -onetime -backend etcd -node http://etcd-server:2379 
-cat /tmp/myapp.conf
-nginx -g 'daemon off;' -c /tmp/myapp.conf
-
+cat /tmp/*.conf
+nginx -g 'daemon off; include /tmp/*.conf;'
--- etcdCurl/insertkeys.sh
+++ etcdCurl/insertkeys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#curl -L http://etcd-server:2379/version
-curl -L http://etcd-server:2379/v2/keys/myapp/subdomain -XPUT -d value="localhost"
-curl -L http://etcd-server:2379/v2/keys/myapp/location -XPUT -d value="get-time"
-curl -L http://etcd-server:2379/v2/keys/myapp/proxytarget -XPUT -d value="py-app:8081/dtime"
-#curl -L http://etcd-server:2379/v2/keys/myapp/upstream/app2 -XPUT -d value="10.0.1.100:80"
-#curl -L http://etcd-server:2379/v2/keys/myapp/upstream/app1 -XPUT -d value="10.0.1.101:80"
+curl -L http://etcd-server:2379/v2/keys/tapp/ti/location -XPUT -d value="get-time"
+curl -L http://etcd-server:2379/v2/keys/tapp/ti/proxytarget -XPUT -d value="py-app:8081/dtime"
+curl -L http://etcd-server:2379/v2/keys/tapp/gr/location -XPUT -d value="be-greeted"
+curl -L http://etcd-server:2379/v2/keys/tapp/gr/proxytarget -XPUT -d value="py-app:8081/greet"
+curl -L http://etcd-server:2379/v2/keys/tapp/st/location -XPUT -d value="www"
+curl -L http://etcd-server:2379/v2/keys/tapp/st/root -XPUT -d value="/var"
+curl -L http://etcd-server:2379/v2/keys/tapp/st/index -XPUT -d value="website.html"
